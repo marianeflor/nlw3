8d9ab9d03ff09d770869ebb14d1f3130ccf73d05
Mariane Flor <marianeflor@gmail.com>
2020-10-15 15:28:17 -0300

    mobile - orphanagesMap page, routes

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const OrphanageDetails = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default OrphanageDetails
+
+const styles = StyleSheet.create({})
